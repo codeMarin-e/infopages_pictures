ab2664b96a63fce5fa1875c75a093e817bfc454c
Marin <marin@dev.nddesign.no>
2023-02-28 15:16:34 +0200

    fix mapping

--- src/hooks/map.php
+++ src/hooks/map.php
@@ -1,8 +1,4 @@
 <?php
-
-use Marinar\InfopagesPictures\MarinarInfopagesPictures;
-
-$packageDir = MarinarInfopagesPictures::getPackageMainDir();
 return [
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'app', 'Models', 'Infopage.php']) => [
         "// @HOOK_TRAITS" => "\tuse \\App\\Traits\\InfopagePicturesTrait; \n",
@@ -11,11 +7,11 @@ return [
         "// @HOOK_INFOPAGES_CONFIGS_ADDONS" => "\t\t\\Marinar\\InfopagesPictures\\MarinarInfopagesPictures::class, \n",
     ],
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'resources', 'views', 'admin', 'infopages', 'infopage.blade.php']) => [
-        "{{-- @HOOK_INFOPAGE_AFTER_CONTENT --}}" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGE_AFTER_CONTENT.blade.php']),
+        "{{-- @HOOK_INFOPAGE_AFTER_CONTENT --}}" => implode(DIRECTORY_SEPARATOR, [__DIR__,  'HOOK_INFOPAGE_AFTER_CONTENT.blade.php']),
     ],
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'app', 'Http', 'Controllers', 'Admin', 'InfopageController.php']) => [
-        "// @HOOK_INFOPAGES_STORE_END" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGES_STORE_END.php']),
-        "// @HOOK_INFOPAGES_UPDATE_END" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGES_STORE_END.php']),
+        "// @HOOK_INFOPAGES_STORE_END" => implode(DIRECTORY_SEPARATOR, [__DIR__,  'HOOK_INFOPAGES_STORE_END.php']),
+        "// @HOOK_INFOPAGES_UPDATE_END" => implode(DIRECTORY_SEPARATOR, [__DIR__,  'HOOK_INFOPAGES_STORE_END.php']),
     ],
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'lang', 'en', 'admin', 'infopages', 'infopage.php']) => [
         "// @HOOK_INFOPAGE_LANG" => "\t'pictures' => [ \n\t\t'label' => 'Pictures', \n\t], \n",
@@ -24,6 +20,6 @@ return [
         "// @HOOK_INFOPAGES_VALIDATION_LANG" => "\t'pictures.*.mime' => 'Wrong file type', \n",
     ],
     implode(DIRECTORY_SEPARATOR, [ base_path(), 'app', 'Http', 'Requests', 'Admin', 'InfopageRequest.php']) => [
-        "// @HOOK_INFOPAGE_REQUEST_RULES" => implode(DIRECTORY_SEPARATOR, [$packageDir, 'hooks', 'HOOK_INFOPAGE_REQUEST_RULES.php']),
+        "// @HOOK_INFOPAGE_REQUEST_RULES" => implode(DIRECTORY_SEPARATOR, [__DIR__,  'HOOK_INFOPAGE_REQUEST_RULES.php']),
     ],
 ];
